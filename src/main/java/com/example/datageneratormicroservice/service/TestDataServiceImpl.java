542f750480f27a8fe4c6f8c8c5d8bd7dbcda8b11
Stanislaav Kozh <printks@yandex.ru>
2024-03-17 15:50:45 +0300

    Initial commit

---
+++ src/main/java/com/example/datageneratormicroservice/service/TestDataServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.datageneratormicroservice.service;
+
+import com.example.datageneratormicroservice.model.Data;
+import com.example.datageneratormicroservice.model.test.DataTestOptions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class TestDataServiceImpl{
+
+   
+}
